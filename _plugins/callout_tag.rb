674c8fd63986869780ebf31dd4541293947ab72f
Sam Meech-Ward <sam@meech-ward.com>
2018-10-15 06:11:59 -0700

    Add more callouts

--- _plugins/callout_tag.rb
+++ _plugins/callout_tag.rb
@@ -15,6 +15,13 @@ module Jekyll
     end
   end
 
+  class SideNoteTag < MyLiquidTag
+    def render(context)
+      html = renderMarkdown @text
+      "<div class=\"action\">#{html}</div>"
+    end
+  end
+
   class AttentionTag < MyLiquidTag
     def render(context)
       html = renderMarkdown @text
@@ -35,10 +42,19 @@ module Jekyll
       "<div class=\"action\">NOTE: #{html}</div>"
     end
   end
+
+  class AsideTag < MyLiquidTag
+    def render(context)
+      html = renderMarkdown @text
+      "<aside class=\"aside\"><p>#{html}</p></aside>"
+    end
+  end
 end
 
 Liquid::Template.register_tag('action', Jekyll::ActionTag)
 Liquid::Template.register_tag('note', Jekyll::NoteTag)
+Liquid::Template.register_tag('side-note', Jekyll::SideNoteTag)
 Liquid::Template.register_tag('attention', Jekyll::AttentionTag)
 Liquid::Template.register_tag('warning', Jekyll::WarningTag)
 Liquid::Template.register_tag('important', Jekyll::ImportantTag)
+Liquid::Template.register_tag('aside', Jekyll::AsideTag)
